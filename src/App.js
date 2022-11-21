0c61d17b0128614b6897ad9969a0be978fbad560
kalyaharish <kalyaharish@gmail.com>
2022-11-21 19:04:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h2> E-Commerce Application... coming soon....</h2>
+    <input type="text"></input>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import Footer from "../../Components/Shared/Footer/Footer";
+import Header from "../../Components/Shared/Header/Header";
+
+
+
+function Home(){
+    return(
+        <div>
+            <Header />
+        <div>This is home Page </div>
+             <Footer />
+        </div>
+    );
+}
+export default Home;
